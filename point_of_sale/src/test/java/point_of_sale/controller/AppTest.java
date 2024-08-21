0485ab52f7c786f9ce3cef27a8ebec546dda4515
jdcanas <justincanas16@gmail.com>
2024-08-21 10:19:06 -0700

    intial commit

---
+++ point_of_sale/src/test/java/point_of_sale/controller/AppTest.java
@@ -0,0 +1,11 @@
+package point_of_sale.controller;
+
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+public class AppTest {
+    @Test public void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
