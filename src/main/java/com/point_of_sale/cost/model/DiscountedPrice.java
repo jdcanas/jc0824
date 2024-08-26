9aa9a093b925a8794d7bab53800368664e51dfba
jdcanas <justincanas16@gmail.com>
2024-08-26 16:44:06 -0700

    formatting of output

--- src/main/java/com/point_of_sale/cost/model/DiscountedPrice.java
+++ src/main/java/com/point_of_sale/cost/model/DiscountedPrice.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public record DiscountedPrice(BigDecimal originalPrice, BigDecimal finalPrice, BigDecimal discountAmount, Integer discountPercent) {
     public void print() {
         System.out.println("Pre-discount charge: $" + originalPrice);
-        System.out.println("Discount percent: " + discountPercent);
+        System.out.println("Discount percent: " + discountPercent + "%");
         System.out.println("Discount amount: $" + discountAmount);
         System.out.println("Final Charge: $" + finalPrice);
     }
